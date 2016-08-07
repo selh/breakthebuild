f519f55b70e8f033a0d12b586dec1a7e8fe25747
Janik Andreas <janik.paul.andreas@sap.com>
2016-08-06 23:27:22 -0700

    No siginifiant improvements.

--- noname.py
+++ noname.py
@@ -2,9 +2,12 @@
 #Authors: Happy Hour Squad
 
 from gpiozero import MotionSensor
+import time
 
-pir = MotionSensor(4)
+#pir = MotionSensor(pin=4,queue_len=10,sample_rate=1.0,threshold=0.5,partial=False)
+pir2 = MotionSensor(4) 
 
 while True:
-	if pir.motion_detected:
+	if pir2.motion_detected:
 		print("Motion detected!")
+		time.sleep(1)
