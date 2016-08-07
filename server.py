8983c208077da80166e46babc0797fce26ee9dcc
seleena <seleena_hua@hotmail.com>
2016-08-06 22:54:14 -0700

    protype endpoint

--- server.py
+++ server.py
@@ -1,11 +1,33 @@
 import web
+import pymongo
+import json
 
-urls= ( '/', 'index')
+
+#calls index method only if url in format /'some character string' --only accepts characters
+urls= ( '/(\w+)', 'index')
 
 class index:
-	def GET(self):
-		return "Hello World"
 
-app = web.application(urls, globals())
+	def GET(self, room_name):
+		#connect to mongodb
+		from pymongo import MongoClient
+		client = MongoClient('mongodb://admin:admin0@ds145405.mlab.com:45405/breakthebuild?authMechanism=SCRAM-SHA-1')
+		db = client['breakthebuild']
+		collection = db.test_collection
+			
+		ret_val = collection.find_one({'name': room_name})		
 
-app.run()
+		#check if a value was returned, else will error out when trying to read hash
+		if ret_val:
+			if ret_val["occupied"] == False:
+				return "Free\n"
+		else:
+			return "Occupied\n"
+		
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
+
+web.config.debug = False
+
